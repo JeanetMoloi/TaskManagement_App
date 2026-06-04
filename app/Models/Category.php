9158c43c1319d684e148cba70b27f5b6cf3cacf5
Jeanet Moloi <230186904@mycput.ac.za>
2026-06-04 23:28:08 +0200

    feat: add relationships and fillable to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
