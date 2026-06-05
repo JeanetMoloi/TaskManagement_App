cef044f27cdd8576f23c2f4f4e8b4d14f57f2b33
Jeanet Moloi <230186904@mycput.ac.za>
2026-06-05 12:40:18 +0200

    Adding Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,4 +1,28 @@
-protected $middleware = [
-    // ... existing middleware
-    \App\Http\Middleware\SecureHeaders::class,
-];
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     */
+    protected $middleware = [
+        \App\Http\Middleware\SecureHeaders::class,
+    ];
+
+    /**
+     * The application's route middleware groups.
+     */
+    protected $middlewareGroups = [
+        'web' => [],
+        'api' => [],
+    ];
+
+    /**
+     * The application's middleware aliases.
+     */
+    protected $middlewareAliases = [];
+}
